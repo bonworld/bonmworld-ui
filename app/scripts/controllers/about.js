e1df163008ec244936828742c0ebd2bb81741671
Bonythomasv <bonythomasv@gmail.com>
2017-04-03 18:02:00 -0500

    test commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name bonmworldUiApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the bonmworldUiApp
+ */
+angular.module('bonmworldUiApp')
+  .controller('AboutCtrl', function ($scope) {
+    $scope.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
